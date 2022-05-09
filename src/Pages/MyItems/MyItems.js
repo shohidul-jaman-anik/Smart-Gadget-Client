525531fa2e93ff1b3f767e9614c3c9c9ac5b69b9
Md Anik Hossain <anikh499@gmail.com>
2022-05-09 16:03:40 +0600

    done

--- src/Pages/MyItems/MyItems.js
+++ src/Pages/MyItems/MyItems.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import axios from 'axios';
 import auth from '../firebase.init';
+import { Flip } from 'react-reveal';
 
 const MyItems = () => {
     const [user] = useAuthState(auth)
@@ -19,11 +20,22 @@ const MyItems = () => {
             setItems(data);
         }
         getItems()
-        
     }, [user])
     return (
         <div>
             <h1>my items {items.length}</h1>
+            <div  className='service-container shadow-sm p-2 text-center rounded-3' data-aos="fade-down"
+        data-aos-easing="linear"
+        data-aos-duration="1500">
+
+            <img src={items.picture} alt="" />
+            <h2>{items.name}</h2>
+            <h4>Price: {items.price}</h4>
+            <h4>Quantity : {items.Quantity}</h4>
+           
+            <p>Supplier : {items.supplier}</p>
+            <Flip right cascade><p>{items.description}</p></Flip>
+        </div>
         </div>
     );
 };
